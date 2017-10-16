8c9db628504f541e1d492adea9a26d2a08693749
Joseph Brown <brownje@rose-hulman.edu>
2017-10-15 20:59:50 -0400

    Fixed spacing...

--- src/main/java/edu/rosehulman/quota/SetConfigController.java
+++ src/main/java/edu/rosehulman/quota/SetConfigController.java
@@ -6,7 +6,7 @@ import spark.Route;
 
 public class SetConfigController implements Route {
 	public Object handle(Request request, Response response) throws Exception {
-		// TODO  check for malformed json/null or empty and send then throw exception
+		// TODO check for malformed json/null or empty and send then throw exception
 		String body = request.body();
 
 		Database.getInstance().setConfig(body);
