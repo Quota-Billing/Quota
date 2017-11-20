2654accfe271e90840fd3ceaf1d77a0ac82408a8
Jack Hallam <hallamja@rose-hulman.edu>
2017-11-20 16:51:07 -0600

    bug found

--- src/main/java/edu/rosehulman/quota/client/SharedServiceClient.java
+++ src/main/java/edu/rosehulman/quota/client/SharedServiceClient.java
@@ -1,5 +1,9 @@
 package edu.rosehulman.quota.client;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import edu.rosehulman.quota.SystemConfig;
+
 public class SharedServiceClient {
 
   private static SharedServiceClient instance;
@@ -14,9 +18,8 @@ public class SharedServiceClient {
   public boolean addUser(String partnerId, String productId, String userId) {
     try {
       // TODO: Put Shared Server path in config and in here
-      // HttpResponse<String> response = Unirest.post(SystemConfig.getInstance().getSharedServerPath() + "/partner/{partnerId}/product/{productId}/user/{userId}").routeParam("partnerId", partnerId).routeParam("productId", productId).routeParam("userId", userId).asString();
-      // return response.getStatus() == 200;
-      return true;
+       HttpResponse<String> response = Unirest.post(SystemConfig.getInstance().getSharedServerPath() + "/partner/{partnerId}/product/{productId}/user/{userId}").routeParam("partnerId", partnerId).routeParam("productId", productId).routeParam("userId", userId).asString();
+       return response.getStatus() == 200;
     } catch (Exception e) {
       return false;
     }
@@ -25,9 +28,8 @@ public class SharedServiceClient {
   public boolean deleteUser(String partnerId, String productId, String userId) {
     try {
       // TODO: Put Shared Server path in config and in here
-      // HttpResponse<String> response = Unirest.delete(SystemConfig.getInstance().getSharedServerPath() + "/partner/{partnerId}/product/{productId}/user/{userId}").routeParam("partnerId", partnerId).routeParam("productId", productId).routeParam("userId", userId).asString();
-      // return response.getStatus() == 200;
-      return true;
+       HttpResponse<String> response = Unirest.delete(SystemConfig.getInstance().getSharedServerPath() + "/partner/{partnerId}/product/{productId}/user/{userId}").routeParam("partnerId", partnerId).routeParam("productId", productId).routeParam("userId", userId).asString();
+       return response.getStatus() == 200;
     } catch (Exception e) {
       return false;
     }
