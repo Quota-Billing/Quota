42c21cd451fdb7fa41e17973cde8e986bd81e064
Joseph Brown <brownje@rose-hulman.edu>
2017-12-16 21:09:02 -0500

    Changed tabs back to spaces.  Removed static import

--- src/main/java/edu/rosehulman/quota/Logging.java
+++ src/main/java/edu/rosehulman/quota/Logging.java
@@ -7,24 +7,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Logging {
-	static final Logger errorLog = LogManager.getLogger("errorLog");
-	static final Logger accessLog = LogManager.getLogger("accessLog");
-	
-	public static void accessLog(String message) {
-		// So as to not generate millions of logs during benchmark testing, access logging has been disabled.
-		// TODO
-		//accessLog.debug(message);
-	}
-	
-	public static void errorLog(String message) {
-		errorLog.error(message);
-	}
-	
-	public static void errorLog(Exception e) {
-		  StringWriter sw = new StringWriter();
-		  e.printStackTrace(new PrintWriter(sw));
-		  String sStackTrace = sw.toString(); // stack trace as a string
-		  errorLog.error(sStackTrace);
-	}
+  private static final Logger errorLog = LogManager.getLogger("errorLog");
+
+  public static void errorLog(String message) {
+    errorLog.error(message);
+  }
+
+  public static void errorLog(Exception e) {
+    StringWriter sw = new StringWriter();
+    e.printStackTrace(new PrintWriter(sw));
+    String sStackTrace = sw.toString(); // stack trace as a string
+    errorLog.error(sStackTrace);
+  }
 
 }
