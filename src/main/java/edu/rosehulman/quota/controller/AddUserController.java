68dd4caba4622b916cb91507a669c974a737c605
Joseph Brown <brownje@rose-hulman.edu>
2017-12-17 21:53:11 -0500

    Logging manually now works.  Uses access and error logs.  Spark still not finding properties file

--- src/main/java/edu/rosehulman/quota/controller/AddUserController.java
+++ src/main/java/edu/rosehulman/quota/controller/AddUserController.java
@@ -15,33 +15,31 @@ public class AddUserController implements Route {
 
   @Override
   public Object handle(Request request, Response response) throws Exception {
-    try {
-      String apiKey = request.params(":apiKey");
-      String productId = request.params(":productId");
+    String apiKey = request.params(":apiKey");
+    String productId = request.params(":productId");
 
-      String partnerId = Database.getInstance().getPartnerByApi(apiKey).get().getPartnerId();
+    String partnerId = Database.getInstance().getPartnerByApi(apiKey).get().getPartnerId();
 
-      JsonObject userJsonObject = new JsonParser().parse(request.body()).getAsJsonObject();
-      String userId = userJsonObject.get("id").getAsString();
+    JsonObject userJsonObject = new JsonParser().parse(request.body()).getAsJsonObject();
+    String userId = userJsonObject.get("id").getAsString();
 
-      User user = new User();
-      user.setPartnerId(partnerId);
-      user.setProductId(productId);
-      user.setUserId(userId);
+    User user = new User();
+    user.setPartnerId(partnerId);
+    user.setProductId(productId);
+    user.setUserId(userId);
 
-      Database.getInstance().addUser(user);
+    Database.getInstance().addUser(user);
 
-      // Send the user to Shared
-      boolean sharedRes = SharedServiceClient.getInstance().addUser(partnerId, productId, userId);
-      if (!sharedRes) {
-        HttpException e = new HttpException("Adding user to shared server failed");
-        Logging.errorLog(e);
-        throw e;
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-      throw e;
+    // Send the user to Shared
+    boolean sharedRes = SharedServiceClient.getInstance().addUser(partnerId, productId, userId);
+    if (!sharedRes) {
+      HttpException e = new HttpException("Adding user to shared server failed");
+      Logging.errorLog(e);
+      //throw e;
     }
+    /*
+     * } catch (Exception e) { e.printStackTrace(); throw e; }
+     */
     return "";
   }
 }
