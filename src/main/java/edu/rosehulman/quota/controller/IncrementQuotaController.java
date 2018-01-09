515f3bbc698dfddf5594c621a78cf3aa6f46c728
Joseph Brown <brownje@rose-hulman.edu>
2018-01-08 20:50:10 -0500

    Prevent frozen user from incrementing.  Also refactored a bit by adding methods to UserTier and Tier that were cluttering up the controller

--- src/main/java/edu/rosehulman/quota/controller/IncrementQuotaController.java
+++ src/main/java/edu/rosehulman/quota/controller/IncrementQuotaController.java
@@ -1,20 +1,14 @@
 package edu.rosehulman.quota.controller;
 
 import edu.rosehulman.quota.Database;
-import edu.rosehulman.quota.Logging;
-import edu.rosehulman.quota.client.BillingClient;
 import edu.rosehulman.quota.model.Tier;
 import edu.rosehulman.quota.model.UserTier;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
-import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
-
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import static spark.Spark.halt;
 
@@ -41,49 +35,7 @@ public class IncrementQuotaController implements Route {
     }
     UserTier userTier = userTierOptional.get();
 
-    // TODO: For now we use BigInteger as the data type for the value and max
-    BigInteger value = new BigInteger(userTier.getValue());
-    BigInteger max = new BigInteger(firstTier.getMax());
-    BigInteger graceExtra = new BigInteger(firstTier.getGraceExtra());
-    BigInteger maxPlusGraceExtra = max.add(graceExtra);
-
-    // See if we are at or above the quota already
-    if (value.compareTo(maxPlusGraceExtra) >= 0) {
-      // TODO should we only send once or send if above also just to be safe?
-      // send to billing
-      String bill = BillingClient.getInstance().quotaReached(partnerId, productId, userId, quotaId, userTier.getTierId());
-      if (bill != null) {
-        throw halt(403, bill);
-      } else {
-        Logging.errorLog("Quota reached Billing endpoint failed");
-        throw halt(500);
-      }
-    }
-
-    // See if adding one to the quota would go above the quota
-    BigInteger incrementedValue = value.add(BigInteger.ONE);
-    if (!request.body().isEmpty()) {
-      JsonObject partnerJsonObject = new JsonParser().parse(request.body()).getAsJsonObject();
-      incrementedValue = value.add(new BigInteger(partnerJsonObject.get("count").getAsString()));
-    }
-    if (incrementedValue.compareTo(maxPlusGraceExtra) > 0) {
-      // send to billing
-      String bill = BillingClient.getInstance().quotaReached(partnerId, productId, userId, quotaId, userTier.getTierId());
-      if (bill != null) {
-        throw halt(403, bill);
-      } else {
-        Logging.errorLog("Quota reached Billing endpoint failed");
-        throw halt(500);
-      }
-    }
-
-    // Save the new value to the database
-    userTier.setValue(incrementedValue.toString());
-    boolean updated = Database.getInstance().updateUserTier(userTier);
-
-    if (!updated) {
-      throw halt(500);
-    }
+    userTier.increment(firstTier, request);
 
     return "";
   }
--- src/main/java/edu/rosehulman/quota/model/UserTier.java
+++ src/main/java/edu/rosehulman/quota/model/UserTier.java
@@ -1,7 +1,17 @@
 package edu.rosehulman.quota.model;
 
+import java.math.BigInteger;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
+
+import edu.rosehulman.quota.Database;
+import edu.rosehulman.quota.Logging;
+import edu.rosehulman.quota.client.BillingClient;
+import spark.Request;
+import static spark.Spark.halt;
 
 @DatabaseTable(tableName = "user_tier")
 public class UserTier {
@@ -70,5 +80,49 @@ public class UserTier {
 
   public void setValue(String value) {
     this.value = value;
+  }
+
+  public void increment(Tier firstTier, Request request) throws Exception {
+    // TODO: For now we use BigInteger as the data type for the value and max
+    BigInteger value = new BigInteger(this.value);
+    BigInteger max = firstTier.calculateMax();
+
+    // See if we are at or above the quota already
+    if (value.compareTo(max) >= 0) {
+      // TODO should we only send once or send if above also just to be safe?
+      // send to billing
+      String bill = BillingClient.getInstance().quotaReached(partnerId, productId, userId, quotaId, this.tierId);
+      if (bill != null) {
+        throw halt(403, bill);
+      } else {
+        Logging.errorLog("Quota reached Billing endpoint failed");
+        throw halt(500);
+      }
+    }
+
+    // See if adding one to the quota would go above the quota
+    BigInteger incrementedValue = value.add(BigInteger.ONE);
+    if (!request.body().isEmpty()) {
+      JsonObject partnerJsonObject = new JsonParser().parse(request.body()).getAsJsonObject();
+      incrementedValue = value.add(new BigInteger(partnerJsonObject.get("count").getAsString()));
+    }
+    if (incrementedValue.compareTo(max) > 0) {
+      // send to billing
+      String bill = BillingClient.getInstance().quotaReached(this.partnerId, this.productId, this.userId, this.quotaId, this.tierId);
+      if (bill != null) {
+        throw halt(403, bill);
+      } else {
+        Logging.errorLog("Quota reached Billing endpoint failed");
+        throw halt(500);
+      }
+    }
+
+    // Save the new value to the database
+    this.value = incrementedValue.toString();
+    boolean updated = Database.getInstance().updateUserTier(this);
+
+    if (!updated) {
+      throw halt(500);
+    }
   }
 }
