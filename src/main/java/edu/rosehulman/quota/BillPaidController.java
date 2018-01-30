ec4e8e638398912763cb6ec7dc35c3a5ce73c91d
Joseph Brown <brownje@rose-hulman.edu>
2018-01-29 19:24:46 -0500

    Added BillPaid controller and changed billing endpoints to use partnerId

--- src/main/java/edu/rosehulman/quota/BillPaidController.java
+++ src/main/java/edu/rosehulman/quota/BillPaidController.java
@@ -1,27 +1,25 @@
-package edu.rosehulman.quota.controller;
+package edu.rosehulman.quota;
 
-import edu.rosehulman.quota.Database;
+import static spark.Spark.halt;
+
+import java.math.BigInteger;
+import java.util.Optional;
+
 import edu.rosehulman.quota.model.UserTier;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
-import java.math.BigInteger;
-import java.util.Optional;
-
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import static spark.Spark.halt;
-
-public class SetQuotaController implements Route {
+public class BillPaidController implements Route {
 
   @Override
   public Object handle(Request request, Response response) throws Exception {
-    String partnerId = request.params(":partnerId");
+    String apiKey = request.params(":apiKey");
     String productId = request.params(":productId");
     String userId = request.params(":userId");
     String quotaId = request.params(":quotaId");
+
+    String partnerId = Database.getInstance().getPartnerByApi(apiKey).get().getPartnerId();
 
     Optional<UserTier> userTierOptional = Database.getInstance().getUserTier(partnerId, productId, userId, quotaId);
     if (!userTierOptional.isPresent()) {
@@ -29,10 +27,6 @@ public class SetQuotaController implements Route {
     }
     UserTier userTier = userTierOptional.get();
     BigInteger resetValue = BigInteger.ZERO;
-    if (!request.body().isEmpty()) {
-      JsonObject partnerJsonObject = new JsonParser().parse(request.body()).getAsJsonObject();
-      resetValue = (new BigInteger(partnerJsonObject.get("count").getAsString()));
-    }
 
     // Save the new value to the database
     userTier.setValue(resetValue.toString());
@@ -44,4 +38,5 @@ public class SetQuotaController implements Route {
 
     return "";
   }
+
 }
