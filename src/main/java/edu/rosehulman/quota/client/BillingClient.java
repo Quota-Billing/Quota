74b9a4ea41f102f8ad4fccf23477135a6460f4cc
Jack Hallam <hallamja@rose-hulman.edu>
2018-03-21 17:12:46 -0400

    Int test

--- src/main/java/edu/rosehulman/quota/client/BillingClient.java
+++ src/main/java/edu/rosehulman/quota/client/BillingClient.java
@@ -1,9 +1,5 @@
 package edu.rosehulman.quota.client;
 
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
-import edu.rosehulman.quota.SystemConfig;
-
 public class BillingClient {
   private static BillingClient instance;
 
@@ -15,25 +11,27 @@ public class BillingClient {
   }
 
   public String quotaReached(String partnerId, String productId, String userId, String quotaId, String tierId) {
-    try {
-      // TODO: Do something besides return null when not found
-      HttpResponse<String> response = Unirest.post(SystemConfig.getInstance().getBillingServerPath() + "/partner/{partnerId}/product/{productId}/user/{userId}/quotaReached/{quotaId}/tier/{tierId}").routeParam("partnerId", partnerId).routeParam("productId", productId).routeParam("userId", userId).routeParam("quotaId", quotaId).routeParam("tierId", tierId).asString();
-      if (response.getStatus() == 200) {
-        return response.getBody();
-      } else {
-        return null;
-      }
-    } catch (Exception e) {
-      return null;
-    }
+//    try {
+//      // TODO: Do something besides return null when not found
+//      HttpResponse<String> response = Unirest.post(SystemConfig.getInstance().getBillingServerPath() + "/partner/{partnerId}/product/{productId}/user/{userId}/quotaReached/{quotaId}/tier/{tierId}").routeParam("partnerId", partnerId).routeParam("productId", productId).routeParam("userId", userId).routeParam("quotaId", quotaId).routeParam("tierId", tierId).asString();
+//      if (response.getStatus() == 200) {
+//        return response.getBody();
+//      } else {
+//        return null;
+//      }
+//    } catch (Exception e) {
+//      return null;
+//    }
+    return "";
   }
 
   public boolean setUserTier(String partnerId, String productId, String quotaId, String tierId, String userId) {
-    try {
-      HttpResponse<String> response = Unirest.post(SystemConfig.getInstance().getBillingServerPath() + "/partner/{partnerId}/product/{productId}/quota/{quotaId}/tier/{tierId}/user/{userId}").routeParam("partnerId", partnerId).routeParam("productId", productId).routeParam("userId", userId).routeParam("quotaId", quotaId).routeParam("tierId", tierId).asString();
-      return response.getStatus() == 200;
-    } catch (Exception e) {
-      return false;
-    }
+//    try {
+//      HttpResponse<String> response = Unirest.post(SystemConfig.getInstance().getBillingServerPath() + "/partner/{partnerId}/product/{productId}/quota/{quotaId}/tier/{tierId}/user/{userId}").routeParam("partnerId", partnerId).routeParam("productId", productId).routeParam("userId", userId).routeParam("quotaId", quotaId).routeParam("tierId", tierId).asString();
+//      return response.getStatus() == 200;
+//    } catch (Exception e) {
+//      return false;
+//    }
+    return true;
   }
 }
